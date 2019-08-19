ef2fbce2e043e2a0524f61611daac3d77b019721
Aleksandar Radulovic <aleksandar_radulovic@rocketmail.com>
2019-08-19 11:53:56 +0200

    Updates

---
+++ precache-manifest.09bfed9782178fc4fe02e91d311cb88f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21c1a19601299dacdf78ff22c75e38d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65fa46b1f3c665ab735f",
+    "url": "/static/css/main.9e34c5a3.chunk.css"
+  },
+  {
+    "revision": "f169344431944fd4169b",
+    "url": "/static/js/2.61813cb1.chunk.js"
+  },
+  {
+    "revision": "65fa46b1f3c665ab735f",
+    "url": "/static/js/main.862687d1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
