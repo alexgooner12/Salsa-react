28357881ee3d7e0b52b7539142b968f5dd8f911f
Aleksandar Radulovic <aleksandar_radulovic@rocketmail.com>
2019-08-19 12:10:31 +0200

    Updates

---
+++ precache-manifest.09b319b865938d8403562b2292cefde8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5957b3abe7fced61176e6d1b0d86e6",
+    "url": "/Salsa-react/index.html/index.html"
+  },
+  {
+    "revision": "11432a71cecc04574118",
+    "url": "/Salsa-react/index.html/static/css/main.9e34c5a3.chunk.css"
+  },
+  {
+    "revision": "31d7bf658c380f360628",
+    "url": "/Salsa-react/index.html/static/js/2.09e2f915.chunk.js"
+  },
+  {
+    "revision": "11432a71cecc04574118",
+    "url": "/Salsa-react/index.html/static/js/main.61658401.chunk.js"
+  },
+  {
+    "revision": "7fa5beec1a3ddbd83992",
+    "url": "/Salsa-react/index.html/static/js/runtime~main.2c057b9f.js"
+  }
+]);
